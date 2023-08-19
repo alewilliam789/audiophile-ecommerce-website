3fbb20a0d4652a96842427078ae21cb835f708b0
alewilliam789 <alewilliam789@gmail.com>
2023-08-19 09:58:42 -0700

    Add Sass & configure mixins/functions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
-const nextConfig = {}
+const nextConfig = {
+    sassOptions: {
+        includePaths: ['./src'],
+        prependData: '@use "@/app/scss/abstracts/mixins" as mixins; @use "@/app/scss/abstracts/functions" as functions;'
+    }
+}
 
 module.exports = nextConfig
