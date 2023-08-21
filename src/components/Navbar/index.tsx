80d8ae1234a7cb07a115f0613f0e0b92c9d5e439
alewilliam789 <alewilliam789@gmail.com>
2023-08-21 12:02:54 -0700

    Switch layout on Navbar from grid to flex

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -12,11 +12,11 @@ export default function Navbar(){
     const linkNames = ["HOME", "HEADPHONES","SPEAKERS", "EARPHONES"]
 
     const links = linkNames.map((linkName)=>{
-        return <Link href={`/${linkName == 'HOME' ? "": `${linkName.toLowerCase()}`}`}>{linkName}</Link>
+        return <Link key={linkName} href={`/${linkName == 'HOME' ? "": `${linkName.toLowerCase()}`}`}>{linkName}</Link>
     })
 
     return (
-            <nav className={`${styles['c-navbar']} ${styles['l-grid']}`}>
+            <nav className={`${styles['c-navbar']} ${styles['l-flex']}`}>
                 {links}
             </nav>
     )
