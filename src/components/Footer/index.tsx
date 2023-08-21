5ce075cd6d80051ab41e5273641f4d2e2c0c69d0
alewilliam789 <alewilliam789@gmail.com>
2023-08-21 12:05:56 -0700

    Style footer for both desktop and tablet screens

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import Image from 'next/image';
 
 
 import Navbar from '../Navbar';
+
+import useScreenWidth from '@/hooks/useScreenWidth';
 
 import audiophileLogo from '../../assets/shared/desktop/logo.svg';
 import facebookLogo from '../../assets/shared/desktop/icon-facebook.svg';
@@ -12,32 +16,47 @@ import styles from './styles.module.scss';
 
 
 
+
 export default function Footer(){
+
+    const width = useScreenWidth();
+
+    function SocialMediaLinks(){
+        return (
+            <>
+                <div className={`${styles['l-flex--social-links']} ${styles['l-grid__item--social-links']}`}>
+                            <Image priority src={facebookLogo} alt='Check out our Facebook'/>
+                            <Image priority src={twitterLogo} alt='Check out our Twitter'/>
+                            <Image priority src={instagramLogo} alt='Check out our Instagram'/>
+                </div>
+            </>
+        )
+    }
+
+
+
+
     return (
         <>
-            <footer className={styles.footer}>
-                <div className={`${styles['footer-container']}`}>
-                    <section className='flex justify-between flex-wrap'>
-                        <Image priority src={audiophileLogo} alt='Audiophile Logo' />
+            <footer className={`${styles[`c-footer`]} ${styles['c-footer--primary']} ${styles['l-flex--footer']}`}>
+                <div className={`${styles['c-footer__content']} ${styles['l-grid']}`}>
+                    <Image priority className={`${styles['c-audiophile-logo']}`} src={audiophileLogo} alt='Audiophile Logo' />
+                    <div className={`${styles['c-navbar']} ${styles['l-grid__item--nav']}`}>
                         <Navbar />
-                    </section>
-                    <section className={`flex justify-between`}>
-                            <p>
-                                Audiophile is an all in one stop to fulfill your
-                                audio needs. We're a small team of music lovers
-                                and sound specialists who are devoted to helping
-                                you get the most out of personal audio. Come and 
-                                visit our demo facility - we're open 7 days a week.
-                            </p>
-                            <div className={`flex ${styles['footer-social-media-container']}`}>
-                                <Image priority src={facebookLogo} alt='Check out our Facebook'/>
-                                <Image priority src={twitterLogo} alt='Check out our Twitter'/>
-                                <Image priority src={instagramLogo} alt='Check out our Instagram'/>
-
-                            </div>
-                    </section>
-                    <sub>Copyright 2021. All Rights Reserved</sub>
-
+                    </div>
+                    <p className={`${styles['l-grid__item--footer-text']}`}>
+                        Audiophile is an all in one stop to fulfill your
+                        audio needs. We're a small team of music lovers
+                        and sound specialists who are devoted to helping
+                        you get the most out of personal audio. Come and 
+                        visit our demo facility - we're open 7 days a week.
+                    </p>
+                    {width>=1300 && <SocialMediaLinks />}
+                    <div className={`${styles['c-subcontainer']} ${styles['l-flex--mobile']} ${styles['l-grid__item']}`}>
+                        <sub>Copyright 2021. All Rights Reserved</sub>
+                        {(width <= 1300 && width>=768) && <SocialMediaLinks />}
+                    </div>
+                    {width <768 && <SocialMediaLinks />}
                 </div>
             </footer>
         </>
